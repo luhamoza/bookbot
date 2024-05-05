4b0bc73d533f7e48d546b3e07436f04c14b7d882
luhamoza <luqmanhafizimohdzawawi@gmail.com>
2024-05-05 11:11:57 +0800

    feat: get word count from the text string

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 def main():
     book_path = "books/frankenstein.txt"
     book_content = get_book_content(book_path)
-    print(book_content)
+    word_count = get_word_count(book_content)
+    # print(book_content)
+    print(f"{word_count} words found in the book")
 
 def get_book_content(path):
     with open(path) as f:
         return f.read()
+
+def get_word_count(str):
+    return len(str.split())
 
 main()
