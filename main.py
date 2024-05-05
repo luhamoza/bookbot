916a97cb6932501d2e93de229766655772ff594c
luhamoza <luqmanhafizimohdzawawi@gmail.com>
2024-05-06 01:03:58 +0800

    feat: add print report to terminal about word count and letter count

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
+from letter_dictionary import letter_dict 
+
 def main():
     book_path = "books/frankenstein.txt"
     book_content = get_book_content(book_path)
     word_count = get_word_count(book_content)
-    letter_count_dict = letter_dict(book_content)
-    print(f"{word_count} words found in the book")
-    print(letter_count_dict)
-
+    print_report(book_path, book_content, word_count)
 
 def get_book_content(path):
     with open(path) as f:
@@ -17,40 +16,23 @@ def get_word_count(str):
 def get_letter_count(letter, alphabet):
     return letter.lower().count(alphabet)
 
-def letter_dict(text):
-    letter_dict = {
-        "a": 0,
-        "b": 0,
-        "c": 0,
-        "d": 0,
-        "e": 0,
-        "f": 0,
-        "g": 0,
-        "h": 0,
-        "i": 0,
-        "j": 0,
-        "k": 0,
-        "l": 0,
-        "m": 0,
-        "n": 0,
-        "o": 0,
-        "p": 0,
-        "q": 0,
-        "r": 0,
-        "s": 0,
-        "t": 0,
-        "u": 0,
-        "v": 0,
-        "w": 0,
-        "x": 0,
-        "y": 0,
-        "z": 0,
-    }
+def populate_letter_dict(text):
     for (key, value) in letter_dict.items():
         if key in letter_dict:
             count = get_letter_count(text, key)
             letter_dict[key] = count
     return letter_dict
 
+def print_report(path, content, count):
+    header = f"--- Begin report of {path} ---"
+    footer = "--- End report ---"
+    populated_letter_dict = populate_letter_dict(content)
+
+    print(header)
+    print(f"{count} words found in the book")
+    print()
+    for (key, value) in populated_letter_dict.items():
+        print(f"The '{key}' character was found {value} times")
+    print(footer)
 
 main()
